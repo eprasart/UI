553d22c5e2034dba969dedae14ad88bb182af094
Visal <visalchea@gmail.com>
2014-07-19 19:48:41 +0700

    TextboxPlaceHolder

---
+++ TextBoxPlaceHolder.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace kPrasat.UI
+{
+    partial class TextBoxPlaceHolder
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label1.Font = new System.Drawing.Font("Noto Sans Khmer", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Noto Sans Khmer", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(364, 25);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // TextBoxPlaceHolder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "TextBoxPlaceHolder";
+            this.Size = new System.Drawing.Size(364, 25);
+            this.EnabledChanged += new System.EventHandler(this.TextBoxPlaceHolder_EnabledChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ TextBoxPlaceHolder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kPrasat.UI
+{
+    [DefaultProperty("Text")]
+    public partial class TextBoxPlaceHolder : UserControl
+    {
+        [Browsable(true)]
+        public event EventHandler TextChanged;
+
+        public TextBoxPlaceHolder()
+        {
+            InitializeComponent();
+        }
+
+        [Browsable(true)]
+        public override string Text
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+
+        [Browsable(true)]
+        public string PlaceHolder
+        {
+            get { return label1.Text; }
+            set { label1.Text = value; }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            textBox1.Focus();
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            label1.Visible = (textBox1.Text.Length == 0);
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextChanged(sender, e);
+        }
+
+        private void TextBoxPlaceHolder_EnabledChanged(object sender, EventArgs e)
+        {
+            if (Enabled == false)
+                label1.Visible = false;
+            else
+                textBox1_Leave(null, null);
+        }
+    }
+}
